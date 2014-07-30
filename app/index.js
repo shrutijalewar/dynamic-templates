a90718c664f54aee10992ab207a4a69a84dcbb4d
Shruti Sharma <shrutijalewar@gmail.com>
2014-07-30 16:46:37 -0500

    checker and sum

--- app/index.js
+++ app/index.js
@@ -13,11 +13,37 @@ app.use(express.static(__dirname + '/static'));
 app.get('/', function(req,res){
   res.render('home');
 });
+app.get('/checkers', function(req,res){
+  res.render('checkers');
+});
+app.get('/add/:x/:y/:a/:b', function(req,res){
+req.params.a *= 1;
+req.params.b *= 1;
+req.params.x *= 1;
+req.params.y *= 1;
 
+console.log(req.params, req.query);
+
+req.params.fontsize = req.query.fontsize;  
+req.params.color = req.query.color;
+req.params.borderwidth = req.query.borderwidth;
+res.render('sum',req.params);
+});
+
+app.get('/sumlist/:a', function(req,res){
+  var a =req.params.a.split(',');
+  a = a.map(function(x){return x * 1;});
+  var sum = 0;
+  for(var i = 0; i< a.length; i++){
+    sum += a[i];
+  }
+  
+  res.render('sumlist',{a:a, sum:sum, even:req.query.even, odd:req.query.odd});
+  });
 var port = process.env.PORT;
 
 app.listen(port, function(){
-    console.log('Express is now listening on PORT', port);
+    console.log('Express is now listening on PORT');
 });
 
 
